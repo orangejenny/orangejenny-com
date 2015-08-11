88dcbc73cb97b6d186f9d1c93106d8234a178e92
Jenny Schweers <orange.jenny@gmail.com>
2015-08-10 20:00:19 -0400

    Switch portfolio.css to LESS

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+// Run "grunt watch" from public/ directory
+module.exports = function(grunt) {
+	grunt.initConfig({
+		// running `grunt less` will compile once
+		less: {
+			development: {
+				options: {
+					paths: ["./css"],
+					yuicompress: true
+				},
+			files: {
+				"./css/portfolio.css": "./css/portfolio.less",
+			}
+		}
+	},
+	watch: {
+		files: "./css/*.less",
+		tasks: ["less"]
+	}
+});
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
