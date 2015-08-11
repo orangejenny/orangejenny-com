b061035e662f6ed4392c5ea26ed891c01d0de137
Jenny Schweers <orange.jenny@gmail.com>
2015-08-10 20:49:33 -0400

    Convert to LESS: nest selectors

--- index.php
+++ index.php
@@ -235,4 +235,3 @@
 		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 	</script>
-	<script type="text/javascript"
