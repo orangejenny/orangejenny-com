889afce23f0a88f56f8a42a9d247d972b1a1d4d2
Jenny Schweers <orange.jenny@gmail.com>
2019-08-18 16:06:11 -0400

    Removed unused 'degree' logic

--- resume.php
+++ resume.php
@@ -38,13 +38,7 @@
 	<hr>
 
 	<div class="left">
-		<!--h3>I have the following <span class="emphasize">experience</span></h3-->
-		<?php $degree = 0 ?>
 		<?php foreach ($experiences as $experience) { ?>
-			<?php if (!$degree && isset($experience->degree)) { ?>
-			 	<!--h3>And have been <span class="emphasize">well-educated</span></h3-->
-			<?php } ?>
-			<?php $degree = isset($experience->degree) ?>
 			<h4><?php echo $experience->heading ?></h4>
 			<ul class="horizontal">
 				<?php foreach ($experience->subheaders as $subheader) { ?>
